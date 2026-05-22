57477e58994430c3674443ba02b76ace8ddec44d
banshaoyan <1480356307@qq.com>
2026-05-22 12:10:57 +0800

    Fix: add admin_router export

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -3,5 +3,6 @@ from app.routers.auth import router as auth_router
 from app.routers.api_keys import router as api_keys_router
 from app.routers.billing import router as billing_router
 from app.routers.gateway import router as gateway_router
+from app.routers.admin import router as admin_router
 
-__all__ = ["auth_router", "api_keys_router", "billing_router", "gateway_router"]
+__all__ = ["auth_router", "api_keys_router", "billing_router", "gateway_router", "admin_router"]
